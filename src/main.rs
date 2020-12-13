80fa87dccd5ef668bb75f70d7b06cdf1150049da
Tony Smith <tony@tonysmith.io>
2020-12-12 20:07:11 -0600

    Added random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
+use rand::Rng;
 
 
 fn main() {
     use std::io::{self, Write};
     println!("\nIt's the Number Guessing Game!\n");
+    let secret_num = rand::Rng().gen_range(0,101);
+    println!("Here's the Secret# {} ",secret_num);
     let mut guess = String::new();
     print!("Please enter your guess: ");
     io::stdout().flush();
