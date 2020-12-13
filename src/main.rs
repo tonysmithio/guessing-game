717862acccd00bf44fb5876666b512dd8eda005d
Tony Smith <tony@tonysmith.io>
2020-12-13 12:24:21 -0600

    Updated lline 12: added '.unwrap()'

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
@@ -6,11 +5,11 @@ fn main() {
     use std::io::{self, Write};
     println!("\nIt's the Number Guessing Game!\n");
     let secret_num = rand::thread_rng().gen_range(1,101);
-    //println!("The secret number is: {}\n",secret_num);
+
     loop{
     let mut guess = String::new();
     print!("Please enter your guess: ");
-    io::stdout().flush();
+    io::stdout().flush().unwrap();
     io::stdin().read_line(&mut guess)
       .expect("Failed to read line");
 
