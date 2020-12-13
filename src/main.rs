0e79f966e0b50d21566a564ee9c67692f1da299a
Tony Smith <tony@tonysmith.io>
2020-12-12 19:29:10 -0600

    updated spaceing

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::io;
 
 fn main() {
     use std::io::{self, Write};
-    println!("It's the Number Guessing Game!");
-    
+    println!("\nIt's the Number Guessing Game!\n");
+
     let mut guess = String::new();
     print!("Please enter your guess: ");
     io::stdout().flush();
     io::stdin().read_line(&mut guess)
       .expect("Failed to read line");
-    println!("You guessed: {}",guess);
+    println!("\nYou guessed: {}",guess);
 }
