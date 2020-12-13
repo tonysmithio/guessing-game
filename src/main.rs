343270e513596c30dd943bba29709b3b8bda3d79
Tony Smith <tony@tonysmith.io>
2020-12-12 18:53:11 -0600

    Created stdin option for entering guesses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io
+
+fn main(){
+    println!("Guess The Number");
+    println!("Please enter your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+      .expect("Failed to read line");
+    println!("You guessed: {}",guess);
 }
