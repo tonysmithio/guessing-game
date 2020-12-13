a44afa3824e268320310085b39c18f391dda5848
Tony Smith <tony@tonysmith.io>
2020-12-12 19:24:21 -0600

    updated so no newline on input statement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+#![allow(unused)]
 use std::io;
 
+
 fn main() {
-    println!("Guess The Number");
-    println!("Please enter your guess.");
+    use std::io::{self, Write};
+    println!("It's the Number Guessing Game!");
+    
     let mut guess = String::new();
+    print!("Please enter your guess: ");
+    io::stdout().flush();
     io::stdin().read_line(&mut guess)
       .expect("Failed to read line");
     println!("You guessed: {}",guess);
